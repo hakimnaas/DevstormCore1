01f81f09f833b4b4964ab8f68036cb58911e3734
hakimnaas <abdelhakim.naas@esprit.tn>
2017-03-08 02:16:20 +0100

    create test product MVC

---
+++ src/WebApplication/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class ProductModel
+    {
+        public string nom { get; set; }
+        public string category { get; set; }
+    }
+}
